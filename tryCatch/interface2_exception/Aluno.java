209d862d9d848e19446f300e3cb35255795dc3b3
GuilhermeLyra-30 <guilhermelyra30@gmail.com>
2026-07-05 17:57:49 -0300

    Delete tryCatch/interface2_exception directory

--- tryCatch/interface2_exception/Aluno.java
+++
@@ -1,8 +0,0 @@
-package interface2;
-
-public interface Aluno {
-    double calcMedia() throws NotaInvalidaException;
-    void addNovaNota(double nota) throws NotaInvalidaException;  
-    void removerUltimaNota() throws NotaInvalidaException;
-    double getMaiorNota() throws NotaInvalidaException;
-}
--- tryCatch/interface2_exception/NotaInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package interface2;
-
-public class NotaInvalidaException extends Exception {
-    public NotaInvalidaException(String mensagem){
-        super(mensagem);
-    }
-}
